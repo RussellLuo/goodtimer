a836858d789a44aa4b9724c4b70355dd69854495
RussellLuo <luopeng.he@gmail.com>
2018-09-12 13:28:34 +0800

    Update examples

--- goodtimer_examples_test.go
+++ goodtimer_examples_test.go
@@ -8,10 +8,6 @@ import (
 )
 
 func Example_blockingRead() {
-	// import "fmt"
-	// import "time"
-	// import "github.com/RussellLuo/goodtimer"
-
 	t := time.NewTimer(2 * time.Second)
 	gt := goodtimer.NewGoodTimer(t)
 
@@ -19,18 +15,20 @@ func Example_blockingRead() {
 	if tv := gt.ReadC(); !tv.IsZero() {
 		fmt.Println("The timer fires")
 	}
+
+	// Output:
+	// The timer fires
 }
 
 func Example_nonBlockingRead() {
-	// import "fmt"
-	// import "time"
-	// import "github.com/RussellLuo/goodtimer"
-
 	t := time.NewTimer(2 * time.Second)
 	gt := goodtimer.NewGoodTimer(t)
 
 	// Read from the wrapped timer's channel C, in a non-blocking way
 	if tv := gt.TryReadC(1 * time.Second); tv.IsZero() {
-		fmt.Println("Timed out before the timer fires")
+		fmt.Println("Timed out before the timer firing")
 	}
+
+	// Output:
+	// Timed out before the timer firing
 }
