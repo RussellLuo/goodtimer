0792b78d280c938a8dea95217f96e2f5d3f74b24
RussellLuo <luopeng.he@gmail.com>
2018-09-15 11:27:13 +0800

    Update documentation

--- goodtimer.go
+++ goodtimer.go
@@ -5,7 +5,8 @@ import (
 )
 
 // GoodTimer wraps the standard time.Timer to provide more user-friendly interfaces.
-// NOTE: All the the functions of GoodTimer *should* be used in the same goroutine.
+//
+// **NOTE**: All the the functions of GoodTimer *should* be used in the same goroutine.
 type GoodTimer struct {
 	t    *time.Timer // The actual timer
 	read bool        // Whether t.C has already been read from
@@ -43,10 +44,6 @@ func (gt *GoodTimer) TryReadC(timeout time.Duration) time.Time {
 }
 
 // Reset changes the timer to expire after duration d.
-//
-// Implementation reference:
-// - https://github.com/golang/go/issues/11513#issuecomment-157062583
-// - https://groups.google.com/d/msg/golang-dev/c9UUfASVPoU/tlbK2BpFEwAJ
 func (gt *GoodTimer) Reset(d time.Duration) {
 	gt.Stop()
 	gt.t.Reset(d)
@@ -55,9 +52,6 @@ func (gt *GoodTimer) Reset(d time.Duration) {
 
 // Stop prevents the Timer from firing.
 // It returns true if the call stops the timer, false if the timer has already expired or been stopped.
-//
-// Implementation reference:
-// - https://golang.org/pkg/time/#Timer.Stop
 func (gt *GoodTimer) Stop() bool {
 	stopped := gt.t.Stop()
 	if !stopped && !gt.read {
