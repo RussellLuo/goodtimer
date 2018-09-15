74952b7f361d620e8306b79b58b69999bb65db8a
RussellLuo <luopeng.he@gmail.com>
2018-09-15 17:45:27 +0800

    Add examples of using Stop and Reset

--- goodtimer_examples_test.go
+++ goodtimer_examples_test.go
@@ -11,7 +11,7 @@ func Example_blockingRead() {
 	t := time.NewTimer(2 * time.Second)
 	gt := goodtimer.NewGoodTimer(t)
 
-	// Read from the wrapped timer's channel C
+	// Read from the wrapped timer's channel C.
 	if tv := gt.ReadC(); !tv.IsZero() {
 		fmt.Println("The timer fires")
 	}
@@ -24,11 +24,36 @@ func Example_nonBlockingRead() {
 	t := time.NewTimer(2 * time.Second)
 	gt := goodtimer.NewGoodTimer(t)
 
-	// Read from the wrapped timer's channel C, in a non-blocking way
+	// Read from the wrapped timer's channel C, in a non-blocking way.
 	if tv := gt.TryReadC(1 * time.Second); tv.IsZero() {
 		fmt.Println("Timed out before the timer firing")
 	}
 
 	// Output:
 	// Timed out before the timer firing
+}
+
+func Example_stop() {
+	t := time.NewTimer(2 * time.Second)
+	gt := goodtimer.NewGoodTimer(t)
+
+	// Any operations in the current goroutine.
+
+	if gt.Stop() {
+		fmt.Println("The timer is stopped before firing")
+	}
+
+	// Output:
+	// The timer is stopped before firing
+}
+
+func Example_reset() {
+	t := time.NewTimer(2 * time.Second)
+	gt := goodtimer.NewGoodTimer(t)
+
+	// Any operations in the current goroutine.
+
+	gt.Reset(2 * time.Second)
+
+	// Now you can use the timer gt again.
 }
